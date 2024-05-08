32bddf47fceaaa1853e2b97aaca0c47b2dbd9ecc
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-05-08 13:06:04 +0530

    create a express server to initiate the socket

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,49 @@
 "use strict";
+// ----------------------------------------------------------- http module ------------------------------------------------
 // using the http bundle provide by the NodeJs runtime
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const ws_1 = __importStar(require("ws"));
-const http_1 = __importDefault(require("http"));
-// create a http server 
-const server = http_1.default.createServer(function (request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
-});
-// create a websocket server
-const wss = new ws_1.WebSocketServer({ server });
-let usercount = 0;
-wss.on('connection', function connection(socket) {
-    socket.on('error', console.error);
-    // when there is a message broadcast it to every client
-    socket.on('message', function message(data, isBinary) {
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === ws_1.default.OPEN) {
-                client.send(data, { binary: isBinary });
-            }
-        });
-    });
-    usercount += 1;
-    console.log("user connected ", usercount);
-    socket.send('Hello! Message From Server!!');
-});
-server.listen(8080, function () {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+// import WebSocket, { WebSocketServer } from 'ws';
+// import http from 'http';
+// // create a http server 
+// const server = http.createServer(function(request: any, response: any) {
+//     console.log((new Date()) + ' Received request for ' + request.url);
+//     response.end("hi there");
+// });
+// // create a websocket server
+// const wss = new WebSocketServer({ server });
+// let usercount=0;
+// wss.on('connection', function connection(socket) {
+//   socket.on('error', console.error);
+//   // when there is a message broadcast it to every client
+//   socket.on('message', function message(data, isBinary) {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data, { binary: isBinary });
+//       }
+//     });
+//   });
+//   usercount+=1;
+//   console.log("user connected ",usercount);
+//   socket.send('Hello! Message From Server!!');
+// });
+// server.listen(8080, function() {
+//     console.log((new Date()) + ' Server is listening on port 8080');
+// });
+// ---------------------------------------------express module ---------------------------------------------------------
+// import express from 'express'
+// import { WebSocketServer,WebSocket } from 'ws'
+// const app = express()
+// const httpServer = app.listen(8080, () => {
+//     console.log('Server started on port 8080')
+// })
+// const wss = new WebSocketServer({ server: httpServer });
+// wss.on('connection', function connection(ws) {
+//   ws.on('error', console.error);
+//   ws.on('message', function message(data, isBinary) {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data, { binary: isBinary });
+//       }
+//     });
+//   });
+//   ws.send('Hello! Message From Server!!');
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,66 @@
+// ----------------------------------------------------------- http module ------------------------------------------------
 // using the http bundle provide by the NodeJs runtime
 
-import WebSocket, { WebSocketServer } from 'ws';
-import http from 'http';
+// import WebSocket, { WebSocketServer } from 'ws';
+// import http from 'http';
 
-// create a http server 
-const server = http.createServer(function(request: any, response: any) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
-});
+// // create a http server 
+// const server = http.createServer(function(request: any, response: any) {
+//     console.log((new Date()) + ' Received request for ' + request.url);
+//     response.end("hi there");
+// });
 
 
-// create a websocket server
-const wss = new WebSocketServer({ server });
+// // create a websocket server
+// const wss = new WebSocketServer({ server });
 
-let usercount=0;
-wss.on('connection', function connection(socket) {
-  socket.on('error', console.error);
+// let usercount=0;
+// wss.on('connection', function connection(socket) {
+//   socket.on('error', console.error);
 
-  // when there is a message broadcast it to every client
-  socket.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
-  });
+//   // when there is a message broadcast it to every client
+//   socket.on('message', function message(data, isBinary) {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data, { binary: isBinary });
+//       }
+//     });
+//   });
 
-  usercount+=1;
-  console.log("user connected ",usercount);
-  socket.send('Hello! Message From Server!!');
-});
+//   usercount+=1;
+//   console.log("user connected ",usercount);
+//   socket.send('Hello! Message From Server!!');
+// });
 
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+// server.listen(8080, function() {
+//     console.log((new Date()) + ' Server is listening on port 8080');
+// });
+
+
+
+
+// ---------------------------------------------express module ---------------------------------------------------------
+// import express from 'express'
+// import { WebSocketServer,WebSocket } from 'ws'
+
+// const app = express()
+// const httpServer = app.listen(8080, () => {
+//     console.log('Server started on port 8080')
+    
+// })
+
+// const wss = new WebSocketServer({ server: httpServer });
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('error', console.error);
+
+//   ws.on('message', function message(data, isBinary) {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data, { binary: isBinary });
+//       }
+//     });
+//   });
+
+//   ws.send('Hello! Message From Server!!');
+// });
