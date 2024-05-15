1bedf9a02f850509340704acb2b7a61a0cbb84b1
Sumitkumar104 <parjapatsumit104@gmail.com>
2024-05-15 12:43:35 +0530

    implement the pub -sub using redisClient

--- Redis-pubsub/express-server/index.ts
+++ Redis-pubsub/express-server/index.ts
@@ -1,41 +1,48 @@
-import express from "express";
-import { createClient } from "redis";
+// import express from "express";
+// import { createClient } from "redis";
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-const client = createClient();
-client.on('error', (err) => console.log('Redis Client Error', err));
+// const client = createClient();
+// client.on('error', (err) => console.log('Redis Client Error', err));
 
-app.post("/submit", async (req, res) => {
-    const problemId = req.body.problemId;
-    const code = req.body.code;
-    const language = req.body.language;
+// app.post("/submit", async (req, res) => {
+//     const problemId = req.body.problemId;
+//     const code = req.body.code;
+//     const language = req.body.language;
 
-    try {
+//     try {
 
-        // left push in message queue to be processed by worker .
-        // data is always in form in string so we need to convert it to string before storing it in redis.
-        await client.lPush("problems", JSON.stringify({ code, language, problemId }));
-        // Store in the database
-        res.status(200).send("Submission received and stored.");
-    } catch (error) {
-        console.error("Redis error:", error);
-        res.status(500).send("Failed to store submission.");
-    }
-});
+//         // left push in message queue to be processed by worker .
+//         // data is always in form in string so we need to convert it to string before storing it in redis.
+//         await client.lPush("problems", JSON.stringify({ code, language, problemId }));
+//         // Store in the database
+//         res.status(200).send("Submission received and stored.");
+//     } catch (error) {
+//         console.error("Redis error:", error);
+//         res.status(500).send("Failed to store submission.");
+//     }
+// });
 
-async function startServer() {
-    try {
-        await client.connect();
-        console.log("Connected to Redis");
+// async function startServer() {
+//     try {
+//         await client.connect();
+//         console.log("Connected to Redis");
 
-        app.listen(3000, () => {
-            console.log("Server is running on port 3000");
-        });
-    } catch (error) {
-        console.error("Failed to connect to Redis", error);
-    }
-}
+//         app.listen(3000, () => {
+//             console.log("Server is running on port 3000");
+//         });
+//     } catch (error) {
+//         console.error("Failed to connect to Redis", error);
+//     }
+// }
 
-startServer();
+// startServer();
+
+
+import { PubSubManager } from "./pubSubManager";
+
+setInterval(() => {
+    PubSubManager.getInstance().userSubscribe(Math.random().toString(), "APPL");
+}, 5000)
